e135e2f2c917f488b9239f383534165ed046eb4d
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-09-07 23:27:35 +0800

    added dummy controller functions

---
+++ controllers/authorController.js
@@ -0,0 +1,40 @@
+const Author = require("../models/author");
+const asyncHandler = require("express-async-handler")
+
+//Display list of all authors
+exports.author_list = asyncHandler(async(req,res,next) => {
+    res.send("NOT IMPLEMENTED: Author list")
+})
+
+//Display detail page for specific author
+exports.author_detail = asyncHandler(async (req,res,next) => {
+    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`)
+})
+
+//Display author create form on GET
+exports.author_create_get = asyncHandler(async (req,res,next) => {
+    res.send("NOT IMPLEMENTED: Author create GET")
+})
+
+//Handle author create on POST
+exports.author_create_post = asyncHandler(async (req,res,next) => {
+    res.send("NOT IMPLEMENTED: Author create POST")
+})
+
+//Display author delete form on GET
+exports.author_delete_get = asyncHandler(async (req,res,next) => {
+    res.send("NOT IMPLEMENTED: Author delete GET")
+})
+
+//Handle author delete on POST
+exports.author_delete_post = asyncHandler(async (req,res,next) => {
+    res.send("NOT IMPLEMENTED: Author delete POST")
+})
+
+exports.author_update_get = asyncHandler(async (req,res,next) =>{
+    res.send("NOT IMPLEMENTED: Author update GET")
+})
+
+exports.author_update_post = asyncHandler(async (req,res,next) => {
+    res.send("NOT IMPLEMENTED: Author update POST")
+})
